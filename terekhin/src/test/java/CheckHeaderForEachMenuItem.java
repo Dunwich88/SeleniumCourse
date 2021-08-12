3c7aee7741e35480cc040c4ab407fe05685010ea
Dunwich88 <itxn@mail.ru>
2021-08-12 14:37:20 +0100

    Task 7 is ready.

--- terekhin/src/test/java/CheckHeaderForEachMenuItem.java
+++ terekhin/src/test/java/CheckHeaderForEachMenuItem.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertTrue;
 public class CheckHeaderForEachMenuItem extends TestBase {
     @Test
     public void loginToAdmin() {
+        //Вход в админку
         driver.get("http://localhost/litecart/admin/login.php");
         WebElement username = driver.findElement(By.name("username"));
         WebElement password = driver.findElement(By.name("password"));
@@ -27,13 +28,27 @@ public class CheckHeaderForEachMenuItem extends TestBase {
     @Test
     public void clickAllMenuItems() {
         loginToAdmin();
+        //Создание списка с элементами меню и получения его размера
         List<WebElement> menuItems = driver.findElements(By.id("app-"));
         int menuItemsSize = menuItems.size();
 
-        //TODO: click sub-menu items; assertions;
-        for (int i = 1; i < menuItemsSize; i++) {
-            WebElement element = element = driver.findElement(By.cssSelector("#app-:nth-child(" + i + ")"));
-            element.findElement(By.tagName("a")).click();
+        //Цикл для прохода по всем элементам меню, начинается с единицы, т.к. nth-child начинается с 1
+        for (int i = 1; i <= menuItemsSize; i++) {
+            WebElement menuElement = menuElement = driver.findElement(By.cssSelector("#app-:nth-child(" + i + ")"));
+            menuElement.findElement(By.tagName("a")).click();
+            //Проверка на наличие пунктов подменю
+            if (isElementPresent(By.cssSelector("#app- > ul"))) {
+                    int submenuItemsSize = driver.findElements(By.cssSelector("#app-:nth-child(" + i + ") > ul > li")).size();
+                    //Прокликивание пунктов подменю
+                for (int j = 1; j <= submenuItemsSize; j++) {
+                    WebElement submenuItem = driver.findElement(By.cssSelector("#app-:nth-child(" + i + ") > ul > li:nth-child(" + j + ")" ));
+                    submenuItem.click();
+                    //Проверка наличия заголовка на странице пункта подменю
+                    assertTrue(isElementPresent(By.tagName("h1")));
+                }
+            }
+            //Проверка наличия заголовка на странице пункта меню
+            assertTrue(isElementPresent(By.tagName("h1")));
         }
     }
 }
--- terekhin/src/test/java/TestBase.java
+++ terekhin/src/test/java/TestBase.java
@@ -40,7 +40,7 @@ public class TestBase {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
         tlDriver.set(driver);
         System.out.println(((HasCapabilities) driver).getCapabilities());
