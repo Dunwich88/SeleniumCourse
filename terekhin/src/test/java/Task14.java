cae7b19d62d87fe30c573d48fb864d55871a1457
Dunwich88 <itxn@mail.ru>
2021-08-25 23:32:38 +0100

    Task14 is ready;

---
+++ terekhin/src/test/java/Task14.java
@@ -0,0 +1,43 @@
+import jdk.nashorn.internal.runtime.regexp.joni.ast.StringNode;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+import java.util.List;
+import java.util.Set;
+
+
+public class Task14 extends TestBase {
+
+
+    public ExpectedCondition<String> anyOtherWindowThan(Set<String> oldWindows) {
+        return new ExpectedCondition<String>() {
+            public String apply(WebDriver driver) {
+                Set<String> handles = driver.getWindowHandles();
+                handles.removeAll(oldWindows);
+                return handles.size() > 0 ? handles.iterator().next() : null;
+            }
+        };
+    }
+
+    @Test
+    public void openLinksInNewTabs() {
+        loginToAdminPanel();
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+        driver.findElement(By.cssSelector("a.button")).click();
+
+        List<WebElement> extLinks = driver.findElements(By.cssSelector("i.fa.fa-external-link"));
+
+        for (WebElement we: extLinks) {
+            String mainWindow = driver.getWindowHandle();
+            Set<String> oldWindows = driver.getWindowHandles();
+            we.click();
+            String newWindow = wait.until(anyOtherWindowThan(oldWindows));
+            driver.switchTo().window(newWindow);
+            driver.close();
+            driver.switchTo().window(mainWindow);
+        }
+    }
+}
